67d8922216a55ede126822c0df52014ee6377359
Muetzilla <muetz-github@gasbichler.ch>
2022-04-15 18:54:08 +0200

    Hmm I don't remember how Java EE Projekt works : (

---
+++ src/main/java/dev/muetzilla/zooprojekt/HelloApplication.java
@@ -0,0 +1,9 @@
+package dev.muetzilla.zooprojekt;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
