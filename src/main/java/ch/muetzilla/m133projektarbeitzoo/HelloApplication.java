76d7c867fbd462d81d0d82821e3690f73b4738aa
Muetzilla <muetz-github@gasbichler.ch>
2022-04-18 18:21:09 +0200

    ohh is working now??

---
+++ src/main/java/ch/muetzilla/m133projektarbeitzoo/HelloApplication.java
@@ -0,0 +1,9 @@
+package ch.muetzilla.m133projektarbeitzoo;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
---
+++ src/main/java/ch/muetzilla/m133projektarbeitzoo/HelloResource.java
@@ -0,0 +1,14 @@
+package ch.muetzilla.m133projektarbeitzoo;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("/hello-world")
+public class HelloResource {
+    @GET
+    @Produces("text/plain")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
