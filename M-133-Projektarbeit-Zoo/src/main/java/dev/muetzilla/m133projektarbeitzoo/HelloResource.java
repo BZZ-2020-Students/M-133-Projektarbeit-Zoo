2792adc62de8be5464deba6cae0372e1fee2deab
Muetzilla <muetz-github@gasbichler.ch>
2022-05-03 10:52:44 +0200

    New Projekt

---
+++ M-133-Projektarbeit-Zoo/src/main/java/dev/muetzilla/m133projektarbeitzoo/HelloResource.java
@@ -0,0 +1,14 @@
+package dev.muetzilla.m133projektarbeitzoo;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+
+@Path("/hello-world")
+public class HelloResource {
+    @GET
+    @Produces("text/plain")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
--- src/main/java/ch/muetzilla/m133projektarbeitzoo/HelloApplication.java
+++
@@ -1,9 +0,0 @@
-package ch.muetzilla.m133projektarbeitzoo;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/zoo")
-public class HelloApplication extends Application {
-
-}
